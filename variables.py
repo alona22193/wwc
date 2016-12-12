8e4dd6a125fe5a90f567356061bcdeda0a57863b
alona22193 <alona22193@gmail.com>
2016-12-12 18:30:36 -0500

    variables exercise

---
+++ variables.py
@@ -0,0 +1,8 @@
+name = 'Ariel'
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+
+print(name + ' is ' + str(age) + " years old now, but she'll turn " + str(next_age) + ' in ' + birth_month + '. ')
+print('Her birthstone is ' + birth_stone + '. ')
