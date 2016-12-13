0e7b24aa48018ac63bd1fd0744483ce497d41c5f
alona22193 <alona22193@gmail.com>
2016-12-12 21:49:43 -0500

    conditionals exercise no valid option entered

--- conditionals.py
+++ conditionals.py
@@ -1,3 +1,43 @@
-age = int(raw_input('How old are you?'))
-print(age > 4 and age <11)
-print(age == 5 or age == 6 or age == 7 or age == 8 or age == 9 or age == 10)
+#temp = int(raw_input('What is the temperature?'))
+
+#print('You should bring the following items:')
+#if temp <= 40:
+#    print('Coat')
+#    print('Hat')
+#    print('Gloves')
+#elif temp <= 70:
+#    print('Coat')
+#    print('Hat')
+#else:
+#    print('Nothing!')
+
+
+#meal_price = raw_input('How much was your meal?')
+#tip = float(meal_price) * .20
+#total = tip + float(meal_price)
+#print('You should tip $ ' + str(tip))
+#print('Your total cost would be ' + str(tip + float(meal_price)))
+
+
+meal_price = float(raw_input('How much was your meal? '))
+print('How would you rate the service? ')
+print('a. Not so good')
+print('b. Good')
+print('c. Excellent!')
+chosen_option = raw_input('Choose an option: ')
+
+# Here's where conditionals come in...
+if chosen_option == 'a':
+    percentage = .15
+elif chosen_option == 'b':
+    percentage = .18
+elif chosen_option == 'c':
+    percentage = .20
+else:
+    print('You did not enter a valid option')
+    percentage = .20
+
+tip = meal_price * percentage
+total_price = meal_price + tip
+print('You should tip $' + str(tip))
+print('Your total cost would be $' + str(total_price))
